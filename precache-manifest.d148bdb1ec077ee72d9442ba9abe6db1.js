4b8ebec7796d976fb6c35f09a833ca11c625a68f
Alexandr <Jekyns@yandex.ru>
2020-08-26 19:54:46 +0300

    deploy

---
+++ precache-manifest.d148bdb1ec077ee72d9442ba9abe6db1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac7fa178089218d78cc8a1f1870e607",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1bc6c1d60c180b51df02",
+    "url": "./static/css/main.69696852.chunk.css"
+  },
+  {
+    "revision": "0e5dcf95e1c90b404dcc",
+    "url": "./static/js/2.22e30724.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.22e30724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc6c1d60c180b51df02",
+    "url": "./static/js/main.6fb2a8c6.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "./static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "da7154cc0ce9cb91c3537f97953d132f",
+    "url": "./static/media/background.da7154cc.png"
+  },
+  {
+    "revision": "6145f81ac54627ab4e8fbdf69b46e632",
+    "url": "./static/media/cat.6145f81a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/utils/defaultColors.js
+++
@@ -1,7 +0,0 @@
-export default {
-  defaultColor: '#1698d9',
-  defaultHoverColor: '#2ea8e6',
-  selectedColor: '#d91667',
-  selectedHoverColor: '#e62e7a',
-  disabledColor: '#b3b3b3'
-}
