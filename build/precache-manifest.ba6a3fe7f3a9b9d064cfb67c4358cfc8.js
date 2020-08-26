21e388adba5368f405cfd0b83d2156631d7eba0c
Alexandr <Jekyns@yandex.ru>
2020-08-26 20:42:29 +0300

    deploy

---
+++ build/precache-manifest.ba6a3fe7f3a9b9d064cfb67c4358cfc8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a56f6d3cfa582663d4fcb0d9db97a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35289b62c669d3a42741",
+    "url": "/static/css/main.7a7f5554.chunk.css"
+  },
+  {
+    "revision": "e2a68ae362709ffb618a",
+    "url": "/static/js/2.263a0f67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.263a0f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35289b62c669d3a42741",
+    "url": "/static/js/main.816b7db2.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "da7154cc0ce9cb91c3537f97953d132f",
+    "url": "/static/media/background.da7154cc.png"
+  },
+  {
+    "revision": "6145f81ac54627ab4e8fbdf69b46e632",
+    "url": "/static/media/cat.6145f81a.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
